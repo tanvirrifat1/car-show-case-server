4f718b5197d3605a0d426b9bca453ade3232867a
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-01-29 19:31:28 +0600

    added sroting

--- src/app/modules/car/car.service.ts
+++ src/app/modules/car/car.service.ts
@@ -48,6 +48,14 @@ const GetAllData = async (
     where: whereConditions,
     skip,
     take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: [options.sortOrder],
+          }
+        : {
+            createdAt: 'asc',
+          },
   });
 
   const total = await prisma.car.count();
@@ -61,85 +69,5 @@ const GetAllData = async (
     data: result,
   };
 };
-
-// const GetAllData = async (
-//   filters: IServiceFilterRequest,
-//   options: IPaginationOptions,
-// ) => {
-//   const { size, page, skip } = paginationHelpers.calculatePagination(options);
-
-//   const { searchTerm, ...filterData } = filters;
-
-//   const andConditions = [];
-
-//   if (searchTerm) {
-//     andConditions.push({
-//       OR: serviceSearchableFields.map((field) => ({
-//         [field]: {
-//           contains: searchTerm,
-//           mode: 'insensitive',
-//         },
-//       })),
-//     });
-//   }
-
-//   if (Object.keys(filterData).length > 0) {
-//     andConditions.push({
-//       AND: Object.keys(filterData).map((key) => ({
-//         [key]: {
-//           equals: (filterData as any)[key],
-//         },
-//       })),
-//     });
-//   }
-
-//   const whereConditions: Prisma.CarWhereInput =
-//     andConditions.length > 0 ? { AND: andConditions } : {};
-
-//   const result = await prisma.car.findMany({
-//     where: whereConditions,
-//     skip,
-//     take: size,
-//     orderBy:
-//       options.sortBy && options.sortOrder
-//         ? { [options.sortBy]: options.sortOrder }
-//         : {
-//             createdAt: 'desc',
-//           },
-//   });
-//   const total = await prisma.car.count({
-//     where: whereConditions,
-//   });
-//   return {
-//     meta: {
-//       total,
-//       page,
-//       size,
-//     },
-//     data: result,
-//   };
-// };
-
-// const GetAllData = async (
-//   filters,
-//   options,
-// ): Promise<IGenericResponse<Car[]>> => {
-//   const { page, limit, skip } = paginationHelpers.calculatePagination(options);
-//   const result = await prisma.car.findMany({
-//     skip,
-//     take: limit,
-//   });
-
-//   const total = await prisma.car.count();
-
-//   return {
-//     meta: {
-//       total,
-//       page,
-//       limit,
-//     },
-//     data: result,
-//   };
-// };
 
 export const CarService = { insertIntoDb, GetAllData };
